125684fe6297b3f7c7706da2d10bcbe7f18817ab
RoundBearChoi <roundbeargames@gmail.com>
2024-06-30 20:24:08 +0900

    save

--- pi.py
+++ pi.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import logging
+import math
 
 from datetime import date
 
@@ -9,8 +10,11 @@ def run():
     today = date.today()
     btc = yf.download('BTC-USD', start='2015-01-01', end=today)
 
-    # Calculate the 365-day moving average
-    btc['365_MA'] = btc['Close'].rolling(window=365).mean()
+    # Calculate the moving averages
+    slow = 360
+    quick = math.floor(slow / 3.14159)
+    btc[str(slow) + '_MA'] = btc['Close'].rolling(window=slow).mean() * 2
+    btc[str(quick) + '_MA'] = btc['Close'].rolling(window=quick).mean()
 
     # Create a logger
     logging.basicConfig(filename='matplotlib_styles.log', level=logging.INFO, filemode='w')
@@ -22,8 +26,9 @@ def run():
     # Plot the closing price and the moving average
     plt.style.use ('fivethirtyeight')
     plt.figure(figsize=(14, 7))
-    plt.plot(btc['Close'], label='Bitcoin', linewidth=0.7)
-    plt.plot(btc['365_MA'], label='365-day MA', linewidth=0.7)
+    plt.plot(btc['Close'], label='Bitcoin', linewidth=0.75)
+    plt.plot(btc[str(slow) + '_MA'], label=str(slow) + '-day MA', linewidth=0.9)
+    plt.plot(btc[str(quick) + '_MA'], label=str(quick) + '-day MA', linewidth=0.9)
     plt.title('BTC Moving Averages')
     plt.xlabel('Date')
     plt.ylabel('Price (USD)')
