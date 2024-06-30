adac86ca54dc93044605a7fcb14a0810d0a22c78
RoundBearChoi <roundbeargames@gmail.com>
2024-06-30 20:40:59 +0900

    save

--- pi.py
+++ pi.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import matplotlib.colors as named_colors
 import matplotlib.pyplot as plt
 import logging
 import math
@@ -19,16 +20,21 @@ def run():
     # Create a logger
     logging.basicConfig(filename='matplotlib_styles.log', level=logging.INFO, filemode='w')
 
-    # Log the available styles
+    # Log background styles
     styles = plt.style.available
     logging.info('Available styles in Matplotlib: %s', styles)
+
+    # Log named colors
+    colors = named_colors.CSS4_COLORS
+    for color_name in colors:
+        logging.info('Color name: %s, RGB value: %s', color_name, colors[color_name])
 
     # Plot the closing price and the moving average
     plt.style.use ('fivethirtyeight')
     plt.figure(figsize=(14, 7))
-    plt.plot(btc['Close'], label='Bitcoin', linewidth=0.75)
-    plt.plot(btc[str(slow) + '_MA'], label=str(slow) + '-day MA', linewidth=0.9)
-    plt.plot(btc[str(quick) + '_MA'], label=str(quick) + '-day MA', linewidth=0.9)
+    plt.plot(btc['Close'], label='Bitcoin', color='darkslategray', linewidth=0.3)
+    plt.plot(btc[str(slow) + '_MA'], label=str(slow) + '-day MA', color='crimson', linewidth=0.7)
+    plt.plot(btc[str(quick) + '_MA'], label=str(quick) + '-day MA', color='forestgreen', linewidth=0.7)
     plt.title('BTC Moving Averages')
     plt.xlabel('Date')
     plt.ylabel('Price (USD)')
